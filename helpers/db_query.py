52a8db7689dcfd6f7e40fb3de97bf8266b089446
QA <yaroslav@intraweb.team>
2021-09-30 16:45:25 +0300

    Update db_query.py

--- helpers/db_query.py
+++ helpers/db_query.py
@@ -3,7 +3,7 @@ import psycopg2
 
 class DB:
     def __init__(self):
-        self.host = 'localhost'
+        self.host = '127.0.0.1'
         self.user = 'postgres'
         self.password = 'postgres'
         self.dbname = 'postgres'
